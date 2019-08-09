d38c242424979b8abe52c63b61e7b7eb607935e9
brankohbk <brankoh@gmail.com>
2019-08-09 19:38:49 -0300

    first commit

---
+++ src/main/java/com/codeoftheweb/salvo/Player.java
@@ -0,0 +1,32 @@
+package com.codeoftheweb.salvo;
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String userName;
+    public Player(){}
+
+    public Player(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
